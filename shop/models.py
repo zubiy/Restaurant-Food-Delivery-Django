c02a5d65bbcb42bbee5fe2afa9d05aec2ef65fc8
Nikolay Zubiy <37973156+zubiy@users.noreply.github.com>
2020-10-29 20:58:51 +0300

    Add files via upload

---
+++ shop/models.py
@@ -0,0 +1,55 @@
+import os
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+    
+
+def get_upload_path(instance, filename):
+    #  задаем название файла названием slug`а продукта
+    filename = instance.slug + '.' + filename.split('.')[1]  
+    return os.path.join('images/', filename)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, related_name='products', 
+        on_delete=models.CASCADE
+        )
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    stock = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to=get_upload_path, blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
